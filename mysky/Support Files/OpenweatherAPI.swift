719d85757865ccc0ce7d608c4365a5d866230130
Abhiram0907 <alluriabhiram7@gmail.com>
2021-08-30 16:24:15 -0500

    UI-Update

--- mysky/Support Files/OpenweatherAPI.swift
+++ mysky/Support Files/OpenweatherAPI.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+let url = URL(string: "api.openweathermap.org/data/2.5/weather?q=London&appid=e943561127a35733273a7fb13debcf80")
+
+// get lat and long
+// get humidity from core location
+// Set up corelocation to access from local storage or erlnag / elxir
--- mysky/View Controllers/ViewController.swift
+++ mysky/View Controllers/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-//Outlets
+//Label outlets
     @IBOutlet var Search: UISearchBar!
     @IBOutlet weak var Location: UILabel!
     @IBOutlet weak var HiTemp: UILabel!
@@ -17,17 +17,45 @@ class ViewController: UIViewController {
     @IBOutlet weak var Day_Weather: UILabel!
     @IBOutlet weak var UVindex: UILabel!
     @IBOutlet weak var LowTemp: UILabel!
+    @IBOutlet weak var Humidity: UILabel!
     
-    @IBOutlet weak var Day_Night_Icon: UIImageView!
+//Img outlets
+    @IBOutlet weak var UVI_Icon: UIImageView!
+    @IBOutlet weak var LoTemp_Icon: UIImageView!
+    @IBOutlet weak var HiTemp_Icon: UIImageView!
+//    @IBOutlet weak var Day_Night_Icon: UIImageView!
+    @IBOutlet weak var CurrentTemp_Icon: UIImageView!
+    @IBOutlet weak var Humidity_Icon: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        Location.adjustsFontSizeToFitWidth = true
-        CurrentTemp.textColor = UIColor.darkGray
+        Location.adjustsFontSizeToFitWidth = true
+        CurrentTemp.textColor = UIColor.black
         HiTemp.textColor = UIColor.orange
         LowTemp.textColor = UIColor.systemTeal
+        UVindex.textColor = UIColor.black
+        Humidity.textColor = UIColor.black
         
-        Day_Night_Icon.image = UIImage(named:"day")
+        UVI_Icon.image = UIImage(named: "uvi")
+        LoTemp_Icon.image = UIImage(named: "lotemp")
+        HiTemp_Icon.image = UIImage(named: "hitemp")
+        CurrentTemp_Icon.image = UIImage(named: "day")
+        Humidity_Icon.image = UIImage(named: "humidity")
+        
+//        let hour = Calendar.current.component(.hour, from: Date())
+//
+//        switch hour {
+//            case 1...6:
+//                Day_Night_Icon.image = UIImage(named:"night")
+//            case 7...19:
+//                Day_Night_Icon.image = UIImage(named:"day")
+//            case 20...24:
+//                Day_Night_Icon.image = UIImage(named:"day")
+//
+//            default:
+//                Day_Night_Icon.image = UIImage(named:"day")
+//         }
+        
     }
         
 }
